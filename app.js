06eec5691452ea5aef96513ae226b927b365f036
Gérard TOKO <gerard.toko@dataleon.ai>
2026-06-12 16:45:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,45 @@
 const express = require('express');
 const app = express();
 const User = require('./User'); // Importer le modèle Sequelize
+const md5 = require('md5'); // Assurez-vous d'installer md5 avec npm install md5
 
 app.use(express.json());
 
+const hashPassword = (password) => {
+  // use md5
+  newPassword = password + 'salt'; // Ajouter un sel pour renforcer la sécurité
+  return md5(newPassword); // Remplacez ceci par le mot de passe haché
+}
+
 // Route pour créer un utilisateur
-app.post('/users', async (req, res) => {
-  const { name, email } = req.body;
+app.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: 'Name, email, and password are required' });
+  }
+  if (!/\S+@\S+\.\S+/.test(email)) {
+    return res.status(400).json({ error: 'Invalid email format' });
+  }
+
+  const existingUser = await User.findOne({ where: { email } });
+  if (existingUser) {
+    return res.status(400).json({ error: 'Email already in use' });
+  }
+
+  const passwordEncrypted = hashPassword(password); // Implémentez cette fonction pour chiffrer les mots de passe
   try {
-    const user = await User.create({ name, email });
+    const user = await User.create({ name, email, password: passwordEncrypted }); // Vous pouvez gérer les mots de passe plus tard
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.post('/users', async (req, res) => {
+  const { name, email, password } = req.body;
+  const passwordEncrypted = hashPassword(password);
+  try {
+    const user = await User.create({ name, email, password: passwordEncrypted });
     res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
